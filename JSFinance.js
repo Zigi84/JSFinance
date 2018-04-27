e792af93030be61e23d5b55bc84c3ee5c4ed527e
Igor Milosavljevic <igor.milosavljevic@ubbad.co.yu>
2018-04-27 14:31:28 +0200

    Clean Up added basic testing

--- JSFinance.js
+++ JSFinance.js
@@ -1,8 +1,6 @@
 //JSFinance
 //Copyright 2018 Igor Milosavljevic MIT licence
-//Standardized to MS Excel
 var JSFinance = function () {};
-
 /** 
  *  @function FV Computes the future value with compounding interest rate
  *  @param {number} rate - Rate of interest expressed as a decimal
@@ -12,16 +10,13 @@ var JSFinance = function () {};
  *  @param {number|boolean} when When the payment is due, 1==true at the end of the period 0==false payment at start of the period
  *  @returns {number} Future value
  */
-JSFinance.prototype.FV = function (rate, numPeriods, pmt,pv,when) {
-   let result = 0;
-   if(rate == 0){
-       result = -1*(pv+(numPeriods*pmt))
-   }else{
-       let temp = rate + 1;
-       result = ((1-Math.pow(temp,numPeriods))*(when?temp:1)*pmt/rate - pv*Math.pow(temp,numPeriods))
-   }
-   return result;
+JSFinance.prototype.FV = function (rate, numPeriods, pmt, pv, when) {
+    let result = 0;
+    if (rate == 0) {
+        result = -1 * (pv + (numPeriods * pmt))
+    } else {
+        let temp = rate + 1;
+        result = ((1 - Math.pow(temp, numPeriods)) * (when ? temp : 1) * pmt / rate - pv * Math.pow(temp, numPeriods))
+    }
+    return result;
 }
-let finance = new JSFinance();
-console.log(finance.FV(0.12,10,-200,-500,1));
-console.log(finance.FV(0,10,-200,-500,1)) //2.5000
--- Test/test.js
+++ Test/test.js
@@ -1,11 +1,13 @@
-var JSFinance = require('jsfinance')
+var JSFinance = require("jsfinance")
 var assert = require('assert');
 describe('JSFinance', function () {
-    it('Should calculate the future value correctly', function () {
-        var finance = new JSFinance();
-        assert.equal(finance.fv(0.12, 10, -200, -500, 1), 5483.84);
-        assert.equal(finance.FV(0, 10, -200, -500, 1), 2500);
-        assert.equal(finance.fv(0.12, 10, -200, -500, 0), 5062.67);
-        assert.equal(finance.FV(0, 10, -200, -500, 0), 2500);
+    describe('Future Value FV()', function () {
+        it('Should calculate the future value correctly', function () {
+            let finance = new JSFinance();
+            assert.equal(finance.fv(0.12, 10, -200, -500, 1), 5483.84);
+            assert.equal(finance.FV(0, 10, -200, -500, 1), 2500);
+            assert.equal(finance.fv(0.12, 10, -200, -500, 0), 5062.67);
+            assert.equal(finance.FV(0, 10, -200, -500, 0), 2500);
+        });
     });
 });
