7c3e269bd7b08c52da4323d0c3cf14f2726d4f80
Igor Milosavljevic <igor.milosavljevic@ubbad.co.yu>
2018-04-27 15:18:18 +0200

    Added Present Value

--- JSFinance.js
+++ JSFinance.js
@@ -2,7 +2,8 @@
 //Copyright 2018 Igor Milosavljevic MIT licence
 var JSFinance = function () {};
 /** 
- *  @function FV Computes the future value with compounding interest rate
+ *  Calculates Future Value
+ *  @function FV
  *  @param {number} rate - Rate of interest expressed as a decimal
  *  @param {number} numPeriods - Number of periods
  *  @param {number} pmt - Payment per period
@@ -19,4 +20,32 @@ JSFinance.prototype.FV = function (rate, numPeriods, pmt, pv, when) {
         result = ((1 - Math.pow(temp, numPeriods)) * (when ? temp : 1) * pmt / rate - pv * Math.pow(temp, numPeriods))
     }
     return result;
+}
+/**
+ * Calculates Present Value
+ * @function PV
+ * @param {number} rate
+ * @param {number} numOfPeriods
+ * @param {number} pmt
+ * @param {number} fv
+ * @param {number} when
+ * @returns {Number} Present Value
+ */
+JSFinance.prototype.PV = function (rate, numOfPeriods, pmt, fv, when){
+    let result = 0;
+    if (rate == 0) {
+        result = -1 * ((numOfPeriods * pmt) + fv);
+    } else {
+        let temp = rate + 1;
+        result = (((1 - Math.pow(temp, numOfPeriods)) / rate) * (when ? temp : 1) * pmt - fv) /
+            Math.pow(temp, numOfPeriods);
+    }
+    return result;
+}
+
+if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = JSFinance;
+        module.exports.JSFinance = JSFinance;
+    }
 }
--- Test/test.js
+++ Test/test.js
@@ -1,9 +1,14 @@
-var JSFinance = require("jsfinance")
 var assert = require('assert');
+var chai = require('chai');
+
+
+JSFinance = require('../jsfinance');
+var finance = new JSFinance();
+
 describe('JSFinance', function () {
     describe('Future Value FV()', function () {
         it('Should calculate the future value correctly', function () {
-            let finance = new JSFinance();
+            let finance = new jsfinance();
             assert.equal(finance.fv(0.12, 10, -200, -500, 1), 5483.84);
             assert.equal(finance.FV(0, 10, -200, -500, 1), 2500);
             assert.equal(finance.fv(0.12, 10, -200, -500, 0), 5062.67);
